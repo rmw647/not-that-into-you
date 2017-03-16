628aa468053735a9bf0d70756c57d7ff57670b3f
Rebecca Ward <rmward118@gmail.com>
2017-03-16 14:08:08 -0400

    updated network_graph and generate_clusters functions with comments and small changes to functionality. Updated ipynb to import custom functions and to call functions correctly.

--- network_graph.py
+++ network_graph.py
@@ -1,11 +1,29 @@
+import networkx as nx
+
 def network_graph(nodelist,edgelist,column):
 
-	G=nx.Graph()
+	"""Generates a network graph from a list of
+	   nodes and edges.
+
+	   Parameters
+	   ----------
+	   nodelist: list of nodes
+	   edgelist: list of 0's or 1's indicating existence of connection between
+	   			 nodes
+	   column: column from dataframe indicating cluster assignment
+
+	   Returns
+	   -------
+	   G: a netwworkx graph object
+	   cluster: dictionary of node:cluster assignments
+	"""
+
+	G = nx.Graph()
 	G.add_nodes_from(nodelist)
 	G.add_edges_from(edgelist)
 
-	pos=nx.spring_layout(G)
+	pos = nx.spring_layout(G)
 	nx.set_node_attributes(G,'pos',pos)
-	cluster=dict(zip(nodelist, column))
-	nx.set_node_attributes(G, 'cluster', cluster)
-	return G
+	clusters = dict(zip(nodelist, column))
+	nx.set_node_attributes(G, 'cluster', clusters)
+	return G, clusters
